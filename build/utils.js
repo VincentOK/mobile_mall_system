d268e76f93aa3a19cf1cc6ecd4d0675f480b7149
lianghc <lianghc@time.com>
2018-09-19 18:56:01 +0800

    商家入驻

--- build/utils.js
+++ build/utils.js
@@ -28,15 +28,15 @@ exports.cssLoaders = function (options) {
       sourceMap: options.sourceMap
     }
   }
-  const px2remLoader = {
-    loader: 'px2rem-loader',
-    options: {
-      remUnit: 75//设计稿宽度/10
-    }
-  };
+  // const px2remLoader = {
+  //   loader: 'px2rem-loader',
+  //   options: {
+  //     remUnit: 75//设计稿宽度/10
+  //   }
+  // };
   // generate loader string to be used with extract text plugin
   function generateLoaders (loader, loaderOptions) {
-    const loaders = options.usePostCSS ? [cssLoader, postcssLoader,px2remLoader] : [cssLoader,px2remLoader]
+    const loaders = options.usePostCSS ? [cssLoader, postcssLoader] : [cssLoader]
 
     if (loader) {
       loaders.push({
